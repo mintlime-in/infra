d9706305cb6c9dc94c6142cfb63969f83618e7db
Keshore Durairaj <keshore.durairaj@optum.com>
2024-08-05 19:31:49 +0530

    update

--- installer.sh
+++ installer.sh
@@ -1,21 +1,62 @@
 #!/bin/bash
 
-usage() {
+function usage() {
   cat <<-EOF
   Usage: bash $0 [options]
     options:
       -c  kubecontext
+      -b  build
       -i  install
       -u  uninstall
-      -h  display help
+      -h  help
 EOF
 }
 
-cd $(dirname $0)
+scriptName=$(basename $0 | cut -d"." -f1)
 
-# cat <<-EOF
-#     Using '${kubecontext}' kube-context
-# EOF
+function install_fn() {
+  echo "installing ${scriptName}"
+}
+
+function uninstall_fn() {
+  echo "uninstalling ${scriptName}"
+}
+
+function build_fn() {
+  echo "building ${scriptName}"
+}
+
+build=0
+install=0
+uninstall=0
+while getopts ":ic:buh" opt; do
+  case ${opt} in
+    c)
+      kubecontext=${OPTARG}
+    ;;
+    b)
+      build=1
+    ;;
+    i)
+      install=1
+    ;;
+    u)
+      uninstall=1
+    ;;
+    h)
+      usage
+      exit 0
+    ;;
+  esac
+done
+
+[[ ${kubecontext} == "" ]] && usage && exit 1
+
+echo "kubecontext: ${kubecontext}"
+
+[ ${build} -eq 1 ] && build_fn
+[ ${install} -eq 1 ] && install_fn
+[ ${uninstall} -eq 1 ] && uninstall_fn
 
 # helm repo add mysql-operator https://mysql.github.io/mysql-operator/
 # helm repo update
