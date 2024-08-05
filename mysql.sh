4293472a347cf0081fbb9d05edf7c4871f4a0c58
Keshore Durairaj <keshore.durairaj@optum.com>
2024-08-05 19:18:25 +0530

    testing

--- mysql.sh
+++ mysql.sh
@@ -1,38 +1,49 @@
 #!/bin/bash
 
-kubecontext="docker-desktop"
-
-cat <<-EOF
-    Using '${kubecontext}' kube-context
+usage() {
+  cat <<-EOF
+  Usage: bash $0 [options]
+    options:
+      -c  kubecontext
+      -i  install
+      -u  uninstall
+      -h  display help
 EOF
+}
 
-helm repo add mysql-operator https://mysql.github.io/mysql-operator/
-helm repo update
+cd $(dirname $0)
 
-helm upgrade --install my-mysql-operator mysql-operator/mysql-operator --namespace mysql --create-namespace
+# cat <<-EOF
+#     Using '${kubecontext}' kube-context
+# EOF
 
-helm --kube-context ${kubecontext} upgrade --install my-mysql-innodbcluster mysql-operator/mysql-innodbcluster \
---set credentials.root.password="password" \
---set tls.useSelfSigned=true \
---set serverInstances=1 \
--n mysql --create-namespace
+# helm repo add mysql-operator https://mysql.github.io/mysql-operator/
+# helm repo update
 
-cat <<-EOF | kubectl --context ${kubecontext} --namespace mysql apply -f -
-apiVersion: v1
-kind: Service
-metadata:
-  name: mysql-nodeport
-spec:
-  type: NodePort
-  selector:
-    component: mysqld
-    mysql.oracle.com/cluster: my-mysql-innodbcluster
-    mysql.oracle.com/instance-type: group-member
-    tier: mysql
-  ports:
-    - name: mysql
-      protocol: TCP
-      port: 3306
-      targetPort: 3306
-      nodePort: 30007
-EOF
+# helm upgrade --install my-mysql-operator mysql-operator/mysql-operator --namespace mysql --create-namespace
+
+# helm --kube-context ${kubecontext} upgrade --install my-mysql-innodbcluster mysql-operator/mysql-innodbcluster \
+# --set credentials.root.password="password" \
+# --set tls.useSelfSigned=true \
+# --set serverInstances=1 \
+# -n mysql --create-namespace
+
+# cat <<-EOF | kubectl --context ${kubecontext} --namespace mysql apply -f -
+# apiVersion: v1
+# kind: Service
+# metadata:
+#   name: mysql-nodeport
+# spec:
+#   type: NodePort
+#   selector:
+#     component: mysqld
+#     mysql.oracle.com/cluster: my-mysql-innodbcluster
+#     mysql.oracle.com/instance-type: group-member
+#     tier: mysql
+#   ports:
+#     - name: mysql
+#       protocol: TCP
+#       port: 3306
+#       targetPort: 3306
+#       nodePort: 30007
+# EOF
