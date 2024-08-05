b41326c66673529833c516291f9f36de7cf5c9fd
Keshore Durairaj <keshore.durairaj@optum.com>
2024-08-05 19:52:46 +0530

    update

--- installer.sh
+++ installer.sh
@@ -60,34 +60,3 @@ echo "kubecontext: ${kubecontext}"
 [ ${build} -eq 1 ] && builder_fn
 [ ${install} -eq 1 ] && installer_fn
 [ ${uninstall} -eq 1 ] && uninstaller_fn
-
-# helm repo add mysql-operator https://mysql.github.io/mysql-operator/
-# helm repo update
-
-# helm upgrade --install my-mysql-operator mysql-operator/mysql-operator --namespace mysql --create-namespace
-
-# helm --kube-context ${kubecontext} upgrade --install my-mysql-innodbcluster mysql-operator/mysql-innodbcluster \
-# --set credentials.root.password="password" \
-# --set tls.useSelfSigned=true \
-# --set serverInstances=1 \
-# -n mysql --create-namespace
-
-# cat <<-EOF | kubectl --context ${kubecontext} --namespace mysql apply -f -
-# apiVersion: v1
-# kind: Service
-# metadata:
-#   name: mysql-nodeport
-# spec:
-#   type: NodePort
-#   selector:
-#     component: mysqld
-#     mysql.oracle.com/cluster: my-mysql-innodbcluster
-#     mysql.oracle.com/instance-type: group-member
-#     tier: mysql
-#   ports:
-#     - name: mysql
-#       protocol: TCP
-#       port: 3306
-#       targetPort: 3306
-#       nodePort: 30007
-# EOF
--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,43 @@
 
-function build_fn() {
-  echo "building ${scriptName}..."
+function install_fn() {
+  echo "${kubecontext}"
+
+    helm repo add mysql-operator https://mysql.github.io/mysql-operator/
+    helm repo update
+
+    helm --kube-context ${kubecontext} upgrade --install my-mysql-operator mysql-operator/mysql-operator -n mysql --create-namespace
+
+    helm --kube-context ${kubecontext} upgrade --install my-mysql-innodbcluster mysql-operator/mysql-innodbcluster \
+    --set credentials.root.password="password" \
+    --set tls.useSelfSigned=true \
+    --set serverInstances=1 \
+    -n mysql --create-namespace
+
+cat <<-EOF | kubectl --context ${kubecontext} -n mysql apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: mysql-nodeport
+spec:
+  type: NodePort
+  selector:
+    component: mysqld
+    mysql.oracle.com/cluster: my-mysql-innodbcluster
+    mysql.oracle.com/instance-type: group-member
+    tier: mysql
+  ports:
+    - name: mysql
+      protocol: TCP
+      port: 3306
+      targetPort: 3306
+      nodePort: 30007
+EOF
+}
+
+function uninstall_fn() {
+    kubectl --context ${kubecontext} -n mysql svc mysql-nodeport
+    helm --kube-context ${kubecontext} -n mysql delete my-mysql-operator
+    helm --kube-context ${kubecontext} -n mysql delete my-mysql-innodbcluster
 }
 
 source <(curl https://raw.githubusercontent.com/mintlime-in/kube-infra/main/installer.sh) $@
